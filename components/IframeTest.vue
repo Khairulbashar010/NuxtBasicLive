81ce441f293a0fac21f001c3eb8d78ff4c7601a2
Khairul Bashar <khairul.bashar.rpsu>
2022-07-31 13:50:25 +0600

    import bootstrap

--- components/IframeTest.vue
+++ components/IframeTest.vue
@@ -1,5 +1,6 @@
 <template>
-  <iframe id="iframe" class="w-full" :src="
+  <iframe id="iframe" class="w-full"
+  height="500" width="500" :src="
         `/live.html`"
         scrolling="no" scrollbars="no" frameborder="0" 
         style="height: 100%; width: 100%; min-height: 500px; z-index: 100; overflow: hidden;">
