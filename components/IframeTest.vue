545ad61b427a0ad5b2ac30aa715dc320243b6d3e
Khairul Bashar <khairul.bashar.rpsu>
2022-08-01 13:13:39 +0600

    update scroll

--- components/IframeTest.vue
+++ components/IframeTest.vue
@@ -1,7 +1,7 @@
 <template>
   <iframe id="iframe" class="w-full" :src="
         `/live.html`" scrollbars="no" frameborder="0" 
-        style="height: 100vh; width: 100%; z-index: 100; overflow-y: scroll">
+        style="height: 70vh; width: 100%; z-index: 100; overflow-y: scroll">
     </iframe>
 </template>
 
